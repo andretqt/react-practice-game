7d3c6c082cffd6a24b1de361143962b53433dbff
yung dev <andre.torquati@gmail.com>
2018-09-07 16:46:26 -0400

    images rerender randomized after clicking on them

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -4,6 +4,17 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import images from "./images.json"
 import FriendCard from "./components/FriendCard/";
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
 
 class App extends Component {
   //define the state
@@ -12,6 +23,7 @@ class App extends Component {
   };
   rearrange = (id) => {
     console.log(id);
+    this.setState(shuffle(images))
   }
   render () {
     return (
@@ -19,6 +31,7 @@ class App extends Component {
         <Title>click game</Title>
         {this.state.images.map(image => (
           <FriendCard
+          rearrange={this.rearrange}
           name={image.name}
           image={image.image}
           id={image.id}
