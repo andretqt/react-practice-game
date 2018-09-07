919290a6ca9691102ad37a965ceb681618ca12f1
yung dev <andre.torquati@gmail.com>
2018-09-07 16:15:38 -0400

    initial file setup

---
+++ clicky-game/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title"
+
+class App extends Component {
+  render () {
+    return (
+      <Wrapper>
+        <Title>click game ground work</Title>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
