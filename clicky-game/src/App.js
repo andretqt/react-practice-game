40e980b356522f734fcd9159251f056b90202704
yung dev <andre.torquati@gmail.com>
2018-09-07 18:54:24 -0400

    working on styling of the page

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+// import Title from "./components/Title";
 import images from "./images.json"
 import FriendCard from "./components/FriendCard/";
+import Navbar from "./components/Navbar/";
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
   while (0 !== currentIndex) {
@@ -19,34 +20,35 @@ function shuffle(array) {
 class App extends Component {
   //define the state
   state = {
-    images,
+    images: images,
     score: 0,
     id: '',
     topScore: 0
   };
   rearrange = (id) => {
     if (id !== this.state.id) {
-      console.log(id);
-      this.setState(shuffle(images));
-      this.setState({score: this.state.score + 1});
-      this.setState({id: id});
-      this.setState({topScore: Math.max(this.state.score + 1,this.state.topScore)})
+      this.setState({
+        images: shuffle(images),
+        score: this.state.score + 1,
+        id: id,
+        topScore: Math.max(this.state.score + 1,this.state.topScore)
+      })
     } else {
-      this.setState({score: 0});
-      this.setState(shuffle(images));
-      this.setState({id: id});
+      this.setState({
+        score: 0,
+        images: shuffle(images),
+        id: id
+      })
     }
   }
   render () {
     return (
       <Wrapper>
-        <Title>click game</Title>
-        <div>
-          <h3> Score counter</h3>
-          <p>current id: {this.state.id}</p>
-          <p>Score: {this.state.score}</p>
-          <p>Top Score: {this.state.topScore}</p>
-        </div>
+        {/* <div>
+          Score: {this.state.score}
+          Top Score: {this.state.topScore}
+        </div> */}
+        <Navbar score={this.state.score} topScore={this.state.topScore}/>
         {this.state.images.map(image => (
           <FriendCard
           rearrange={this.rearrange}
--- clicky-game/src/components/FriendCard/FriendCard.js
+++ clicky-game/src/components/FriendCard/FriendCard.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./FriendCard.css";
 
+const styles = {
+  containerStyles: {
+    height: "200px",
+    width: "200px"
+  },
+  cardStyles: {
+    height: "200px",
+    width: "200px"
+  },
+  imageStyles: {
+    height: "200px",
+    width: "200px"
+  }
+}
 const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} id= {props.id} onClick={() => props.rearrange(props.id)}/>
+  <div className="card" style={styles.cardStyles}>
+    <div className="img-container" style={styles.containerStyles}>
+      <img style={styles.imageStyles} alt={props.name} src={props.image} id= {props.id} onClick={() => props.rearrange(props.id)}/>
     </div>
   </div>
 );
